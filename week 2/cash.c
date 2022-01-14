c83fb8d9456ebed5c56e6b7958391a5c30fe496c
Mauricio Bustamante <mauro@MNB002.teaminternational.com>
2022-01-14 10:46:26 -0500

    recover programs

--- week 2/cash.c
+++ week 2/cash.c
@@ -1,30 +1,57 @@
-#include <stdio.h>
+// this program tells you how many coins and which type is needed to optimize change\
+
 #include <cs50.h>
+#include <stdio.h>
 
+int get_cents(void);
+int calculate_quarters(int get_cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
-
-
-int perro(int mauri);
 int main(void)
-{
-    int change;
-    int value = value(change);
-    int quarter= quarter(change);
-
+{ 
+    int cents = get_cents();
+    int quarters = calculate_quarters(cents);
+    int dimes = calculate_dimes(cents);
+    int nickels = calculate_nickels(cents);
+    int pennies = calculate_pennies(cents);
+    printf("\nQuarters: %i\nDimes: %i\nNickels: %i\nPennies: %i \n\n", quarters, dimes, nickels, pennies);
 }
 
-int value(int change)
-{
-    change = get_int("how much change do you need? ");
-    return change;
-}
-int quarter(int change)
-{
+int get_cents(void)
+{   
+    int cents_owed;
+
     do
     {
-        value = change/25;
-    } while (value>25);
+        cents_owed = get_int("\nHow many cents I owed you? ");
+    } while (cents_owed < 0);
 
-    return value;
-    
+    return cents_owed;   
+}
+
+int calculate_quarters(int cents)
+{
+    int quarters = cents / 25;
+    return quarters;
+}
+
+
+int calculate_dimes(int cents)
+{
+    int dimes = (cents % 25) / 10;
+    return dimes;
+}
+
+int calculate_nickels(int cents)
+{
+    int nickels = ((cents % 25) % 10) / 5;
+    return nickels;
+}
+
+int calculate_pennies(int cents)
+{
+    int pennies = (((cents % 25) % 10) % 5) / 1;
+    return pennies;
 }
---
+++ week 2/menu2.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+
+char temp;
+
+int main(void)
+{
+    printf("\nMENÚ:\n1) string to lower case.\n2) string to upper case.\n3) sandwiched.\n4) change letter.\n5) system().\n6) exit\n ");
+
+    int menu = 0;
+    char yn;
+    do 
+    {
+    menu = get_int("chose your number [1-6]: ");
+    if (menu ==6 )
+    {
+        break;
+    }
+    else
+    {
+        continue;
+    }
+    } while (menu <1 || menu >5);
+    
+
+
+    while (menu >0 && menu <7 )
+    {
+        if (menu == 1 )
+        {
+            do
+            {
+            
+                printf("string to lower case.");
+                string st = get_string("type: ");
+                //int len = strlen(st);
+                for (int i=0, len = strlen(st); i< len; i++)
+                {
+                    if (st[i] >= 'A' && st[i] <= 'Z')
+                    {
+                        printf("%c", st[i]+32);
+                    }
+                    else
+                    {
+                        printf("%c", st[i]);
+                    }
+                }
+
+                 yn = get_char("\ndo you want to do it again?");
+            }while(yn == 'y');
+            
+
+        }
+
+        else if(menu == 2 )
+        {
+            do
+            {
+                string st = get_string("type: ");
+                for (int i=0, len = strlen(st); i< len; i++)
+                {
+                    if (st[i] >= 'a' && st[i] <= 'z')
+                    {
+                        printf("%c", st[i]-32);
+                    }
+                    else
+                    {
+                        printf("%c", st[i]);
+                    }
+                }    
+            
+
+            yn = get_char("\ndo you want to do it again? ");
+            }while(yn == 'y');
+
+        }
+
+        else if(menu == 3 )
+        {
+            do
+            {
+            printf("sandwiched.");
+            string st = get_string("type: ");
+             for (int i=0, len = strlen(st); i< len; i++)
+                {
+                    if (st[i] >= 'a' && st[i] <= 'z' && i%2==0)
+                    {
+                        printf("%c", st[i]-32);
+                    }
+                    else 
+                    {
+                        printf("%c", st[i]);
+                    }
+                }    
+
+            yn = get_char("\ndo you want to do it again?");
+            }while(yn == 'y');
+        }
+
+        else if (menu == 4 )
+        {
+            do
+            {
+            //scanf("%c",&temp);
+            char sandwich[50];
+           
+            printf("type a string, the letter that yo want to replace it without spaces (string,letter,character) ");
+            scanf("%[^\n]", sandwich);
+
+
+            char * string = strtok(sandwich,",");
+            char * letter = strtok(NULL,",");
+            char * character = strtok(NULL,",");
+
+            printf("change letter.");
+
+
+
+
+            yn = get_char("do you want to do it again?");
+            }while(yn == 'y');
+
+            
+        }
+        
+
+        else if(menu == 5 )
+        {
+            do
+            {
+            printf("string to upper case.");
+            char command[20];
+            printf("Enter a command ");
+            scanf ("%[^\n]",command);
+            system(command);
+            yn = get_char("do you want to do it again?");
+            }while(yn == 'y');
+        }
+        else if(menu == 6 )
+        {
+            break;
+        }
+        
+        printf("\nMENÚ:\n1) string to lower case.\n2) string to upper case.\n3) sandwiched.\n4) change letter.\n5) system().\n6) exit\n ");
+        menu = get_int("chose your number [1-6]: ");
+
+        
+    }
+
+}
